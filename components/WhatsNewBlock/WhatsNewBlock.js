4ca9d6ff015a30e11bd547157c4860b33848e73b
Artem Orekhov <orehouse@ya.ru>
2022-01-30 18:33:03 +0300

    Refactor WhatsNewBlock.js

--- components/WhatsNewBlock/WhatsNewBlock.js
+++ components/WhatsNewBlock/WhatsNewBlock.js
@@ -1,13 +1,7 @@
-import Image from 'next/image';
-
 import BlockTemplate from '../BlockTemplate/BlockTemplate';
 
 import styles from './WhatsNewBlock.module.scss';
-import WhatsNewBlockFeature from './_Feature/WhatsNewBlockFeature';
-
-import tanya from '../../public/images/whatsnew/Tanya.png';
-import Mark from '../ui/Mark/Mark';
-import Circle, {CIRCLE_COLOR} from '../ui/Circle/Circle';
+import WhatsNewBlockSite from './_Site/WhatsNewBlockSite';
 
 function WhatsNewBlock() {
   return <BlockTemplate>
@@ -15,29 +9,8 @@ function WhatsNewBlock() {
       <div className={styles.whatsNewBlockBulb}/>
       <h5 className={styles.whatsNewBlockLetsStart}>Начнем по порядку</h5>
       <h2 className={styles.whatsNewBlockTitle}>Что изменилось</h2>
-      <WhatsNewBlockFeature title={'Сайт'}>
-        <div className={styles.whatsNewBlockCircle}>
-          <Circle color={CIRCLE_COLOR.DEFAULT_GRADIENT}/></div>
-        <div className={styles.whatsNewBlockSite}>
-          <div>
-            <div className={styles.whatsNewBlockTanyaImg}>
-              <Image src={tanya} alt="Таня"/>
-            </div>
-            <div className={styles.whatsNewBlockTanyaSign}>Дизайн полностью
-              переработала
-              Таня, UX-дизайнер и соосновательница платформы.
-            </div>
-          </div>
-          <div className={styles.whatsNewBlockQuote}>«Сайт и личный кабинет
-            теперь
-            сделаны <Mark>интуитивно понятными:</Mark> мы их разграничили,
-            убрали лишнее и добавили нужное. Теперь на сайте вы можете прочитать
-            информацию о сервисах, тарифах, команде. А в личном кабинете —
-            создавать уроки».
-            <hr align="right"/>
-          </div>
-        </div>
-      </WhatsNewBlockFeature>
+
+      <WhatsNewBlockSite/>
     </div>
   </BlockTemplate>;
 }
---
+++ components/WhatsNewBlock/_Site/WhatsNewBlockSite.js
@@ -0,0 +1,35 @@
+import styles from './WhatsNewBlockSite.module.scss';
+import Circle, {CIRCLE_COLOR} from '../../ui/Circle/Circle';
+import Image from 'next/image';
+import tanya from '../../../public/images/whatsnew/Tanya.png';
+import Mark from '../../ui/Mark/Mark';
+import WhatsNewBlockFeature from '../_Feature/WhatsNewBlockFeature';
+
+function WhatsNewBlockSite() {
+  return <WhatsNewBlockFeature title={'Сайт'}>
+    <div className={styles.whatsNewBlockSiteCircle}>
+      <Circle color={CIRCLE_COLOR.DEFAULT_GRADIENT}/>
+    </div>
+    <div className={styles.whatsNewBlockSite}>
+      <div>
+        <div className={styles.whatsNewBlockSiteTanyaImg}>
+          <Image src={tanya} alt="Таня"/>
+        </div>
+        <div className={styles.whatsNewBlockSiteTanyaSign}>Дизайн полностью
+          переработала
+          Таня, UX-дизайнер и соосновательница платформы.
+        </div>
+      </div>
+      <div className={styles.whatsNewBlockSiteQuote}>«Сайт и личный кабинет
+        теперь
+        сделаны <Mark>интуитивно понятными:</Mark> мы их разграничили,
+        убрали лишнее и добавили нужное. Теперь на сайте вы можете прочитать
+        информацию о сервисах, тарифах, команде. А в личном кабинете —
+        создавать уроки».
+        <hr align="right"/>
+      </div>
+    </div>
+  </WhatsNewBlockFeature>;
+}
+
+export default WhatsNewBlockSite;
