4cfe65e484de46fbc265d9870fa08ba28d70e2b4
Artem Orekhov <orehouse@ya.ru>
2022-02-02 17:18:33 +0300

    Update WhatsNewBlockCabinet component styles

--- components/WhatsNewBlock/_Cabinet/WhatsNewBlockCabinet.js
+++ components/WhatsNewBlock/_Cabinet/WhatsNewBlockCabinet.js
@@ -6,6 +6,9 @@ import WhatsNewBlockFeature, {
   CIRCLE_COLOR,
   CONTENT_ALIGN,
 } from '../_Feature/WhatsNewBlockFeature';
+import BenefitsList from '../../BenefitsList/BenefitsList';
+
+import styles from './WhatsNewBlockCabinet.module.scss';
 
 const topContent = <>
   В меню добавили ваш профиль — <Mark>можете в любой момент посмотреть логин,
@@ -14,15 +17,35 @@ const topContent = <>
 const bottomContent = 'А ещё теперь видна дата, до которой тариф оплачен. Можете оплатить следующий месяц заранее, чтобы не потерять доступ к платным функциям.';
 const title = 'Личный кабинет';
 
+const benefits = [
+  {
+    title: 'Вход по одной кнопке',
+    description: 'Теперь вам не придётся каждый раз вручную вводить логин и пароль — можно заходить по одной кнопке через ваш гугл-аккаунт.',
+  },
+  {
+    title: 'Фильтрация уроков',
+    description: 'Все уроки, которые вы создадите, можно сортировать по сервисам. Раньше они шли подряд, и приходилось искать нужный сервис по всему списку созданных уроков.',
+  },
+  {
+    title: 'Восстановление пароля',
+    description: 'При восстановлении пароля теперь вы сами задаёте нужный. До этого приходилось пользоваться тем, который приходил в письме — поменять было нельзя.',
+  },
+];
+
 function WhatsNewBlockCabinet() {
   return <WhatsNewBlockItem title={title}>
     <WhatsNewBlockFeature
         image={cabinetImg}
+        imageShadow
+        alt="Кабинет"
         contentAlign={CONTENT_ALIGN.LEFT}
         circleColor={CIRCLE_COLOR.DARK_GREEN}
     >
       <TextBlock topContent={topContent} bottomContent={bottomContent}/>
     </WhatsNewBlockFeature>
+    <div className={styles.whatsNewBlockCabinetBenefits}>
+      <BenefitsList benefits={benefits}/>
+    </div>
   </WhatsNewBlockItem>;
 }
 
