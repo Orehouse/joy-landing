67e15c7b70d1a10529271d2fa4bf125a6b0823e0
Artem Orekhov <orehouse@ya.ru>
2022-02-02 11:06:16 +0300

    Add size options to TextBlock component

--- components/TextBlock/TextBlock.js
+++ components/TextBlock/TextBlock.js
@@ -1,10 +1,16 @@
+import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './TextBlock.module.scss';
 
+export const TEXT_BLOCK_SIZE = {
+  MEDIUM: styles.textBlock_medium,
+  LARGE: styles.textBlock_large,
+};
+
 function TextBlock(props) {
-  const {topContent, bottomContent} = props;
-  return <div className={styles.textBlock}>
+  const {topContent, bottomContent, size} = props;
+  return <div className={classnames(styles.textBlock, size)}>
     <div className={styles.textBlockTopContent}>{topContent}</div>
     <div className={styles.textBlockBottomContent}>{bottomContent}</div>
   </div>;
@@ -19,6 +25,11 @@ TextBlock.propTypes = {
     PropTypes.string,
     PropTypes.node,
   ]).isRequired,
+  size: PropTypes.oneOf(Object.values(TEXT_BLOCK_SIZE)),
+};
+
+TextBlock.defaultProps = {
+  size: TEXT_BLOCK_SIZE.MEDIUM,
 };
 
 export default TextBlock;
