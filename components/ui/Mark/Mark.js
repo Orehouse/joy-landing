229d4e0e46a13b4beddefd14fb3baf8c6e529940
Artem Orekhov <orehouse@ya.ru>
2022-02-02 11:02:31 +0300

    Add color options to Mark component

--- components/ui/Mark/Mark.js
+++ components/ui/Mark/Mark.js
@@ -1,7 +1,23 @@
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 import styles from './Mark.module.scss';
 
+export const MARK_COLOR = {
+  YELLOW: styles.markYellow,
+  TURQUOISE_BRIGHT: styles.markTurquoise,
+};
+
 function Mark(props) {
-  return <span className={styles.mark}>{props.children}</span>;
+  const {children, color} = props;
+  return <span className={classnames(styles.mark, color)}>{children}</span>;
 }
+
+Mark.propTypes = {
+  color: PropTypes.oneOf(Object.values(MARK_COLOR)),
+};
+
+Mark.defaultProps = {
+  color: MARK_COLOR.YELLOW,
+};
 
 export default Mark;
